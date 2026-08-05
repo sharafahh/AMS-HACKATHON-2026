2127cc2e7fc523a009171538ed284af24df25ce9
hasanzaki567 <hasanzaki993@gmail.com>
2026-08-05 21:03:58 +0530

    added the line didvider for each page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-﻿import Navbar from "../components/layout/Navbar";
+import Navbar from "../components/layout/Navbar";
 import Hero from "../components/home/Hero";
 import About from "../components/home/About";
 import Stats from "../components/home/Stats";
@@ -9,6 +9,7 @@ import Facilities from "../components/home/Facilities";
 import FAQ from "../components/home/FAQ";
 import Contact from "../components/home/Contact";
 import Footer from "../components/layout/Footer";
+import SectionDivider from "../components/common/SectionDivider";
 
 function Home() {
   return (
@@ -21,26 +22,42 @@ function Home() {
         {/* 1. Hero Section */}
         <Hero />
 
+        <SectionDivider />
+
         {/* 2. About AMS HACKATHON 2026 */}
         <About />
+
+        <SectionDivider />
 
         {/* 3. Hackathon Statistics */}
         <Stats />
 
+        <SectionDivider />
+
         {/* 4. Innovation Tracks (12 Cards) */}
         <Tracks />
+
+        <SectionDivider />
 
         {/* 5. Prize Pool & Rewards */}
         <PrizePool />
 
+        <SectionDivider />
+
         {/* 6. Event Timeline Roadmap */}
         <Timeline />
+
+        <SectionDivider />
 
         {/* 7. Facilities & Infrastructure */}
         <Facilities />
 
+        <SectionDivider />
+
         {/* 8. Frequently Asked Questions */}
         <FAQ />
+
+        <SectionDivider />
 
         {/* 9. Contact & Map */}
         <Contact />
@@ -52,4 +69,4 @@ function Home() {
   );
 }
 
-export default Home;
+export default Home;
